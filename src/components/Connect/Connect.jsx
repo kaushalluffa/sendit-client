0c54900774b48095b7f5cc82a5957e02ab5f3431
kaushalluffa <webdevelopment852@gmail.com>
2023-01-04 16:35:55 -0500

    btns disable if any two services got connected

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -3,15 +3,20 @@ import React, { useEffect, useState } from "react";
 import { FaSpotify, FaApple, FaYoutube } from "react-icons/fa";
 import "./Connect.scss";
 
-const Connect = ({closed,setClosed}) => {
-  const [spotifyConnected, setSpotifyConnected] = useState( JSON.parse(localStorage.getItem('user')).userId ? true : false);
-  const [youtubeConnected, setYoutubeConnected] = useState(false);
+const Connect = ({ closed, setClosed }) => {
+  const [spotifyConnected, setSpotifyConnected] = useState(
+    JSON.parse(localStorage.getItem("spotifyUser"))?.userId ? true : false
+  );
+  const [youtubeConnected, setYoutubeConnected] = useState(
+    JSON.parse(localStorage.getItem("googleUser"))?.userId ? true : false
+  );
   const [appleConnected, setAppleConnected] = useState(false);
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("user")) || {}
   );
-  let windowOpen;
+  const [disableBtn, setDisableBtn] = useState(false);
   function onClickSpotify() {
+    let windowOpen;
     let timer = null;
     windowOpen = window.open(
       "http://localhost:8000/auth/spotify",
@@ -20,18 +25,32 @@ const Connect = ({closed,setClosed}) => {
     );
     if (windowOpen) {
       timer = setInterval(() => {
-        if(windowOpen.closed){
-          fetchUser()
-          if(timer) clearInterval(timer)
-        }
-       }, 500);
-    }
+        if (windowOpen.closed) {
+          fetchUser();
 
-     
+          if (timer) clearInterval(timer);
+        }
+      }, 500);
+    }
   }
 
   function onClickYoutube() {
-    setYoutubeConnected(true);
+    let windowOpen;
+    let timer = null;
+    windowOpen = window.open(
+      "http://localhost:8000/auth/google",
+      "_blank",
+      "widht=500, height=500"
+    );
+    if (windowOpen) {
+      timer = setInterval(() => {
+        if (windowOpen.closed) {
+          fetchUser();
+
+          if (timer) clearInterval(timer);
+        }
+      }, 500);
+    }
   }
   function onClickApple() {
     setAppleConnected(true);
@@ -42,28 +61,50 @@ const Connect = ({closed,setClosed}) => {
       .get("http://localhost:8000/user", { withCredentials: true })
       .then((res) => {
         if (res.data.userId) {
-          setSpotifyConnected(true);
-          setUser(res.data)
-          localStorage.setItem("user", JSON.stringify(res.data));
+          localStorage.setItem(
+            `${res.data.provider}User`,
+            JSON.stringify(res.data)
+          );
+          window.location.reload();
+          setUser(res.data);
         }
       });
   }
- 
+  useEffect(() => {
+    if (
+      (spotifyConnected && youtubeConnected) ||
+      (youtubeConnected && appleConnected)||
+      (spotifyConnected && appleConnected) 
+    ) {
+      setDisableBtn(true);
+    } else {
+      setDisableBtn(false);
+    }
+  }, [spotifyConnected,youtubeConnected,appleConnected]);
 
   return (
     <div className="connect">
       <div className="connect__wrapper">
-        <button className="connect__wrapper--spotify" onClick={onClickSpotify}>
+        <button
+          className="connect__wrapper--spotify"
+          onClick={onClickSpotify}
+          disabled={disableBtn}
+        >
           <FaSpotify /> Spotify {spotifyConnected && "- Connected"}
         </button>
-        <button className="connect__wrapper--youtube" onClick={onClickYoutube}>
+        <button
+          className="connect__wrapper--youtube"
+          onClick={onClickYoutube}
+          disabled={disableBtn}
+        >
           <FaYoutube /> Youtube Music {youtubeConnected && "- Connected"}
         </button>
-        <button className="connect__wrapper--apple" onClick={onClickApple}>
+        <button
+          className="connect__wrapper--apple"
+          onClick={onClickApple}
+          disabled={disableBtn}
+        >
           <FaApple /> Apple Music {appleConnected && "- Connected"}
-        </button>
-        <button style={{ cursor: "pointer" }} onClick={fetchUser}>
-          Fetch User
         </button>
       </div>
     </div>
